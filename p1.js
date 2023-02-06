225b13ddf257cf8fc774b72c61e7e90805a01404
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-07 00:31:38 +0600

    first commit

--- p1.js
+++ p1.js
@@ -1,1 +1,10 @@
-const mindGame = ()
+function mindGame(inputNumber){
+    let sterTech = inputNumber * 3;
+        sterTech = sterTech + 10;
+        sterTech = sterTech / 2;
+        sterTech = sterTech - 5;
+        return sterTech;
+}
+const input = 33;
+const tech = mindGame(input);
+console.log(tech);
